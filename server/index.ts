d739291a45a100a10e774de591f34011798e7b8a
ystepchyn <y.stepchyn@make.com>
2022-11-23 08:52:55 +0100

    refactor server

--- server/index.ts
+++ server/index.ts
@@ -1,39 +1,43 @@
 import express, { Express, Request, Response } from 'express';
-import database from './db';
+import Database from './db';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-const db = new database();
+const db = new Database();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  // curl -X POST 'http://localhost:8000
   res.json({ "todo_tasks": db.getValues() });
 });
 
 app.route('/todo/:id')
   .get((req, res) => {
+    // curl -X POST 'http://localhost:8000/todo/2'
     res.json(db.getRecord(req.params.id));
   })
+
   .post((req, res) => {
-    /*
-      curl -X POST 'http://localhost:8000/todo/2' -H "Content-Type: application/json" -d '{"task": "cleanup the room"}' 
-    */
+    // curl -X POST 'http://localhost:8000/todo/2' -H "Content-Type: application/json" -d '{"task": "cleanup the room"}' 
     let isAdded = db.addRecord({ id: req.params.id, task: req.body.task });
     if (!isAdded) {
-      res.json({ "error": "Record " + req.params.id + " already exists!" });
+      return res.status(400).json({ "error": `Record ${req.params.id} already exists!` });
     }
     res.send(200);
   })
+
   .put((req, res) => {
     // curl -X PUT 'http://localhost:8000/todo/1' -H "Content-Type: application/json" -d '{"task": "buy milk"}' 
     let updated = db.updateRecord({ id: req.params.id, task: req.body.task });
     res.json(updated);
   })
+
   .delete((req, res) => {
+    // curl -X DELETE 'http://localhost:8000/todo/1'
     db.deleteRecord(req.params.id);
     res.send(200);
   })
